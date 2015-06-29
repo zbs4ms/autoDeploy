876c5fdfdac856e5f15965552144c3d115231eb2
zbs <zbs4ms@163.com>
2015-06-29 15:47:47 +0800

    执行脚本

--- src/app/service/callBack.py
+++ src/app/service/callBack.py
@@ -10,6 +10,7 @@ import socket
 class callBack(object):
     def __init__(self,data):
         self.data = data
+        self.ip = data.get("clinetIp")
         self.scriptId = data.get("scriptId")
         self.taskId = data.get("")
         self.script = db_service.Scripts;
@@ -22,6 +23,15 @@ class callBack(object):
     #保存返回信息入库
     def save(self):
         #保存
+        log = self.data.get("logstr")
+        order = self.data.get('order')
+        task=self.task.get_task_one_by_id(self.taskId).result
+        if task and task.get('subtask'):
+            subtask = task.get('subtask')
+            sub = subtask[order]
+            task.update({"id":self.taskId,"subtask.ip":self.ip},"$inc")
+
+
 
     #判断是否成功
     def isSucced(self):
@@ -41,7 +51,12 @@ class callBack(object):
     #下一个需要执行的脚本
     def next(self):
         order = self.data.get('order')
-        task.get_task_one_by_id(self.taskId)
+        task=self.task.get_task_one_by_id(self.taskId).result
+        if task and task.get('subtask'):
+            subtask = task.get('subtask')
+            nextOrder=order+1
+            if len(subtask) < nextOrder:
+                return subtask[nextOrder]
 
     #执行脚本
     def send(self):
